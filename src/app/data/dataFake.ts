70b58595b8e6ad3a667af671f19010c21eff9090
luizk9 <edimirk9@gmail.com>
2023-05-27 04:48:02 -0300

    1-envio ja com projeto parcial

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,31 @@
+export const dataFake = [
+  {
+    id: '1',
+    photoCover:
+      'https://tudosobrecachorros.com.br/wp-content/uploads/yorkshire.jpg',
+    title: 'Tudo começou assim',
+    description: 'Era uma vez um lindo cãozinho cheio de pelos pretos, que cresceu, acho que só em latido, bem vido mana  pra mim é cata-SelectorListContext..."
+  },
+  {
+    id: '2',
+    photo: '',
+    photoCover: '',
+    title: 'huck',
+    description:
+      'Verde e forte, grande e destroi tudo, mais tem um grande coração',
+  },
+  {
+    id: '3',
+    photo: '',
+    photoCover:
+      'https://gkpb.com.br/wp-content/uploads/2022/06/fagWFrNEGrM2p3DCOwgrO5fBmUn-scaled-e1655148584687.jpg',
+    title: 'homen aranha',
+    description: 'Ele é o amigo da vizinhança, faz muita bagunça',
+  },
+  {
+    id: '4',
+    photoCover: '/assets/img/oreudo.webp',
+    title: 'Oreudo - sou eu ',
+    description: 'Esse é o tipo bagunceiro, faz xixi, em tudo e adora lati !',
+  },
+];
